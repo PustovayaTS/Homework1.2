0e9f14b7fc45eee5088ea4b54a6e995c7c36a2ee
PustovayaTS <ais_ta@bk.ru>
2022-09-12 16:11:42 +0700

    Задача 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       byte totalPaint = 120;
+       byte whitePaint = 2;
+       byte brownPaint = 4;
+       int paintForOneClass = whitePaint + brownPaint;
+       int totalClasses = totalPaint / paintForOneClass;
+       int totalWhitePaint = whitePaint * totalClasses;
+       int totalBrownPaint = brownPaint * totalClasses;
+       System.out.println("В школе, где " + totalClasses + " классов, нужно " + totalWhitePaint + " банок белой краски и " + totalBrownPaint + " банок коричневой краски");
     }
 }
