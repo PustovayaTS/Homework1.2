84d995706d021a222df32228bda82f25087bbecc
PustovayaTS <ais_ta@bk.ru>
2022-09-12 15:13:31 +0700

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double vDouble = 22.12;
+        long vLong = 987678965549L;
+        float vFloat = 2.786F;
+        boolean vBoolean = vLong < vFloat;
+        char vChar = 569;
+        short vShort = -159;
+        int vInt = 27897;
+        byte vByte = 67;
     }
 }
