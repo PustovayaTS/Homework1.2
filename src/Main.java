a47a129731e3a4cf865e5a6033cf7c19929eb5e7
PustovayaTS <ais_ta@bk.ru>
2022-09-12 17:04:09 +0700

    Задача 7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       short needToReset = 7 * 1000;
+       int lose250Grams = needToReset / 250;
+       int lose500Grams = needToReset / 500;
+       int averageQuantity  = (lose250Grams + lose500Grams) / 2;
+       System.out.println(lose250Grams);
+       System.out.println(lose500Grams);
+       System.out.println(averageQuantity);
+
     }
 }
